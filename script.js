2188483c0b4a7d597764e28b856702babdbc5438
trialnerr <54381853+trialnerr@users.noreply.github.com>
2023-12-07 16:51:28 -0500

    made some style changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let userScore = 0;
 let computerScore = 0;
-let gameEnded = false; 
+let gameEnded = false;
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
@@ -9,10 +9,10 @@ const compScore = document.querySelector(".compScore");
 const updateElement = document.querySelector("#update");
 const hidden = document.querySelector(".hidden");
 const resetBtn = document.querySelector(".reset");
-const yourChoice = document.querySelector(".your-choice"); 
-const compChoice = document.querySelector(".comp-choice"); 
-const winningMessage = document.querySelector(".winningMessage")
-console.log(hidden, resetBtn); 
+const yourChoice = document.querySelector(".your-choice");
+const compChoice = document.querySelector(".comp-choice");
+const winningMessage = document.querySelector(".winningMessage");
+console.log(hidden, resetBtn);
 
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
@@ -23,14 +23,12 @@ function getComputerChoice() {
 function update(userScore, computerScore) {
   yourScore.textContent = `SCORE:  ${userScore}`;
   compScore.textContent = `SCORE:   ${computerScore}`;
-
 }
 
-function updateSelection(playerSelection, computerSelection){
+function updateSelection(playerSelection, computerSelection) {
   yourChoice.textContent = `You:  ${playerSelection}`;
   compChoice.textContent = `Computer:   ${computerSelection}`;
 }
-
 
 function playRound(playerSelection, computerSelection) {
   switch (playerSelection + computerSelection) {
@@ -64,20 +62,19 @@ function playRound(playerSelection, computerSelection) {
 */
 function showResultMessage(message) {
   winningMessage.textContent = message;
-  
   hidden.style.display = "flex"; // Show the hidden div
 }
 
 function reset() {
   userScore = 0;
   computerScore = 0;
-  update(userScore, computerScore); 
+  update(userScore, computerScore);
   gameEnded = false; // Reset game end flag
   hidden.style.display = "none"; // Hide the hidden div
 }
 
 function play(playerSelection) {
-  if (gameEnded) return; 
+  if (gameEnded) return;
 
   const computerSelection = getComputerChoice();
   const result = playRound(playerSelection, computerSelection);
@@ -87,26 +84,24 @@ function play(playerSelection) {
   } else if (result.includes("lose")) {
     computerScore++;
   }
-  updateSelection(playerSelection, computerSelection); 
+  updateSelection(playerSelection, computerSelection);
   update(userScore, computerScore);
 
   if (userScore === 5) {
-    showResultMessage("You won!"); 
-    gameEnded = true; 
+    showResultMessage("You won!");
+    gameEnded = true;
   }
-  if (computerScore === 5){
-    showResultMessage("You lost!"); 
-    gameEnded = true; 
+  if (computerScore === 5) {
+    showResultMessage("You lost!");
+    gameEnded = true;
   }
 }
 
-
 function main() {
-  resetBtn.addEventListener("click", reset); 
+  resetBtn.addEventListener("click", reset);
   rock.addEventListener("click", () => play("rock"));
   paper.addEventListener("click", () => play("paper"));
   scissors.addEventListener("click", () => play("scissors"));
 }
 
 main();
-
